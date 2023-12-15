47d8cb897af7cb205cedc43c431f9fec9ed00876
lllariogonzalez <lllariogonzalez@gmail.com>
2023-12-15 14:01:42 -0300

    add(App) Home Page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Ofers from './components/Oferts/Ofers'
+import ProcessBuys from './components/ProcessBuys/ProcessBuys'
+import TopCategories from './components/TopCategories/TopCategories'
+import TopSales from './components/TopSales/TopSales'
+import Trends from './components/Trends/Trends'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Ofers />
+      <Trends />
+      <TopCategories />
+      <TopSales />
+      <ProcessBuys />
+      <Footer />
+    </>
+  )
+}
+
+export default App
