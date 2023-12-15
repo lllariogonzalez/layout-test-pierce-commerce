a7c7ce59e9d27b32f85ea76c3cf0616442e13ef6
lllariogonzalez <lllariogonzalez@gmail.com>
2023-12-15 14:09:53 -0300

    add Icons components svg

---
+++ src/components/Icons/GrupoCircleIcon.jsx
@@ -0,0 +1,12 @@
+const GroupCircleIcon = () => {
+  return (
+  <svg xmlns="http://www.w3.org/2000/svg" width="111" height="12" viewBox="0 0 111 12" fill="none">
+    <circle cx="6" cy="6" r="6" fill="white"/>
+    <circle cx="39" cy="6" r="5.25" stroke="white" strokeWidth="1.5"/>
+    <circle cx="72" cy="6" r="5.25" stroke="white" strokeWidth="1.5"/>
+    <circle cx="105" cy="6" r="5.25" stroke="white" strokeWidth="1.5"/>
+  </svg>
+  );
+};
+
+export default GroupCircleIcon;
---
+++ src/components/Icons/GrupoCircleIconMobile.jsx
@@ -0,0 +1,12 @@
+const GroupCircleIconMobile = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="109" height="10" viewBox="0 0 109 10" fill="none">
+      <circle cx="5" cy="5" r="5" fill="white"/>
+      <circle cx="38" cy="5" r="4.25" stroke="white" strokeWidth="1.5"/>
+      <circle cx="71" cy="5" r="4.25" stroke="white" strokeWidth="1.5"/>
+      <circle cx="104" cy="5" r="4.25" stroke="white" strokeWidth="1.5"/>
+    </svg>
+  );
+};
+
+export default GroupCircleIconMobile;
