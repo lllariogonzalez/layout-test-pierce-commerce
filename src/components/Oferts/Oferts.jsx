6786f1286a1b4cb34028db47cf83425fa4d83ad3
lllariogonzalez <lllariogonzalez@gmail.com>
2023-12-15 14:06:50 -0300

    add Oferts component

---
+++ src/components/Oferts/Oferts.jsx
@@ -0,0 +1,12 @@
+import GroupCircleIcon from "../Icons/GrupoCircleIcon";
+import styles from "./Oferts.module.css";
+
+const Ofers = () => {
+  return (
+    <figure className={styles.ofersContainer}>
+      <GroupCircleIcon className={styles.groupIcons} />
+    </figure>
+  );
+};
+
+export default Ofers;
