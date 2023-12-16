c8f6cf6a955fddd740a66c0e2cf1d6097b809680
lllariogonzalez <lllariogonzalez@gmail.com>
2023-12-16 00:44:54 -0300

    add responsive TopSales design

--- src/components/TopSales/TopSales.jsx
+++ src/components/TopSales/TopSales.jsx
@@ -4,8 +4,16 @@ import styles from "./TopSales.module.css";
 const TopSales = () => {
   return (
     <section className={styles.salesContainer}>
-      <h2 className={styles.title}>Las mas vendidas</h2>
+      <h2 className={styles.title}>Los mas vendidos</h2>
       <ListProducts />
+      <div className={styles.nav}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="109" height="10" viewBox="0 0 109 10" fill="none">
+        <circle cx="5" cy="5" r="5" fill="#DC1E0F"/>
+        <circle cx="38" cy="5" r="4.25" stroke="#DC1E0F" strokeWidth="1.5"/>
+        <circle cx="71" cy="5" r="4.25" stroke="#DC1E0F" strokeWidth="1.5"/>
+        <circle cx="104" cy="5" r="4.25" stroke="#DC1E0F" strokeWidth="1.5"/>
+    </svg>
+      </div>
     </section>
   );
 };
