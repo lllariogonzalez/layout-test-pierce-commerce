6e4122cc2f1438e0291896f117976f86d046e958
lllariogonzalez <lllariogonzalez@gmail.com>
2023-12-16 00:45:12 -0300

    add responsive ProcessBuy design

--- src/components/ProcessBuys/ProcessBuys.jsx
+++ src/components/ProcessBuys/ProcessBuys.jsx
@@ -6,7 +6,8 @@ import ProcessCard from './ProcessCard/ProcessCard';
 
 const ProcessBuys = ()=>{
   return (
-    <section className={styles.processBuysContainer}>
+    <section className={styles.container}>
+      <div className={styles.processBuysContainer}>
       <ProcessCard 
         title='Elegí los productos que vas a comprar'
         description='Si querés más de uno, sumalos a tu carrito.'>
@@ -22,6 +23,14 @@ const ProcessBuys = ()=>{
         description='Elegí la forma de entrega que prefieras ¡y listo! aseguramos tu entrega con Mercado Envíos.'>
         <ShippingTruck />
       </ProcessCard>
+      </div>
+      <div className={styles.nav}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="7" viewBox="0 0 45 7" fill="none">
+          <circle cx="3.5" cy="3.5" r="3.5" fill="black"/>
+          <circle cx="22.5" cy="3.5" r="3.25" stroke="black" strokeWidth="0.5"/>
+          <circle cx="41.5" cy="3.5" r="3.25" stroke="black" strokeWidth="0.5"/>
+        </svg>
+      </div>
     </section>
   );
 }
